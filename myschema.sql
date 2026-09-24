b8cf1d21cf33f704a608bc81fadf436bcbdff300
rajushaw01 <rajushaw2741@gmail.com>
2026-09-24 19:35:50 +0530

    Updated schema

--- myschema.sql
+++ myschema.sql
@@ -1,120 +1,104 @@
--- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
---
--- Host: localhost    Database: ecommerce_db
--- ------------------------------------------------------
--- Server version	8.0.46
+-- =========================================================
+-- E-COMMERCE HIBERNATE ASSIGNMENT
+-- schema.sql
+-- =========================================================
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Select the database
+USE ecommerce_db;
 
---
--- Table structure for table `category`
---
 
-DROP TABLE IF EXISTS `category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `category` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `description` varchar(100) DEFAULT NULL,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK46ccwnsi9409t36lurvtyljak` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- =========================================================
+-- 1. CATEGORY
+-- =========================================================
 
---
--- Table structure for table `order_details`
---
+CREATE TABLE IF NOT EXISTS category (
+                                        id INT NOT NULL AUTO_INCREMENT,
+                                        name VARCHAR(50) NOT NULL,
+    description VARCHAR(100),
 
-DROP TABLE IF EXISTS `order_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order_details` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `quantity` int NOT NULL,
-  `unit_price` double NOT NULL,
-  `order_id` int NOT NULL,
-  `product_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKjyu2qbqt8gnvno9oe9j2s2ldk` (`order_id`),
-  KEY `FKinivj2k1370kw224lavkm3rqm` (`product_id`),
-  CONSTRAINT `FKinivj2k1370kw224lavkm3rqm` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  CONSTRAINT `FKjyu2qbqt8gnvno9oe9j2s2ldk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    PRIMARY KEY (id),
 
---
--- Table structure for table `orders`
---
+    CONSTRAINT uk_category_name
+    UNIQUE (name)
+    ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orders` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `order_date` datetime(6) NOT NULL,
-  `total_amount` double NOT NULL,
-  `user_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK32ql8ubntj5uh44ph9659tiih` (`user_id`),
-  CONSTRAINT `FK32ql8ubntj5uh44ph9659tiih` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `product`
---
+-- =========================================================
+-- 2. USERS
+-- =========================================================
 
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `product` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `price` double NOT NULL,
-  `stock_quantity` int DEFAULT NULL,
-  `category_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK1mtsbur82frn64de7balymq9s` (`category_id`),
-  CONSTRAINT `FK1mtsbur82frn64de7balymq9s` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE IF NOT EXISTS users (
+                                     id INT NOT NULL AUTO_INCREMENT,
+                                     username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    role ENUM('ADMIN', 'CUSTOMER') NOT NULL,
 
---
--- Table structure for table `users`
---
+    PRIMARY KEY (id),
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` enum('ADMIN','CUSTOMER') NOT NULL,
-  `username` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`),
-  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+    CONSTRAINT uk_users_username
+    UNIQUE (username),
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+    CONSTRAINT uk_users_email
+    UNIQUE (email)
+    ) ENGINE=InnoDB;
 
--- Dump completed on 2026-09-24 19:23:29
+
+-- =========================================================
+-- 3. PRODUCT
+-- =========================================================
+
+CREATE TABLE IF NOT EXISTS product (
+                                       id INT NOT NULL AUTO_INCREMENT,
+                                       name VARCHAR(50) NOT NULL,
+    price FLOAT(53) NOT NULL,
+    stock_quantity INT,
+    category_id INT NOT NULL,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT fk_product_category
+    FOREIGN KEY (category_id)
+    REFERENCES category(id)
+    ) ENGINE=InnoDB;
+
+
+-- =========================================================
+-- 4. ORDERS
+-- =========================================================
+
+CREATE TABLE IF NOT EXISTS orders (
+                                      id INT NOT NULL AUTO_INCREMENT,
+                                      order_date DATETIME(6) NOT NULL,
+    total_amount FLOAT(53) NOT NULL,
+    user_id INT NOT NULL,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT fk_orders_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ) ENGINE=InnoDB;
+
+
+-- =========================================================
+-- 5. ORDER DETAILS
+-- =========================================================
+
+CREATE TABLE IF NOT EXISTS order_details (
+                                             id INT NOT NULL AUTO_INCREMENT,
+                                             quantity INT NOT NULL,
+                                             unit_price FLOAT(53) NOT NULL,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT fk_order_details_order
+    FOREIGN KEY (order_id)
+    REFERENCES orders(id),
+
+    CONSTRAINT fk_order_details_product
+    FOREIGN KEY (product_id)
+    REFERENCES product(id)
+    ) ENGINE=InnoDB;
